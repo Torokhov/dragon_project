1e7693abce1be668fb995a713835d48361114a7f
Oleg Torokhov <torokhov093@gmail.com>
2017-05-15 20:08:49 +0700

    Create project

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+const webpack = require('webpack');
+
+module.exports = {
+  context: __dirname + '/js',
+  
+  entry: {
+    dragon: "./dragon",
+    game: "./game"
+  },
+    output: {
+      path: __dirname +  "/js",
+      filename: "[name].js",
+      library: "[name]"
+    },
+  
+  watch: NODE_ENV == 'development',
+  
+  watchOptions: {
+    aggregateTimeout: 100
+  },
+  
+  devtool: NODE_ENV == 'development' ? "cheap-inline-module-source-map" : "source-map",
+  
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify(NODE_ENV)
+    })
+  ],
+  
+  resolve: {
+    modulesDirectories: ['node_modules'],
+    extensions: ['', '.js']
+  },
+  
+  resolveLoader: {
+    modulesDirectories: ['node_modules'],
+    moduleTemplates: ['*-loader', '*'],
+    extensions: ['', '.js']
+  },
+  
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel?presets[]=es2015'
+    }]
+  }
+};
+
+ if (NODE_ENV == 'production') {
+    module.exports.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          drop_console: true,
+          unsafe: true
+        }   
+      })
+    );
+  }
